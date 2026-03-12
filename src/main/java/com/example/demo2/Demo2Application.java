7f54d1e052f3d6ebf054d3868cea2a9878d20f44
natiliser <natiliser@gmail.com>
2026-03-12 10:02:58 +0200

    Revert "bad commit"
    This reverts commit d7113e1d125b7dc81c16d5925da5fe652f07cdcd.

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -8,7 +8,6 @@ public class Demo2Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from branch 2 considering branch 1");
-		System.out.println("bad commit");
 		SpringApplication.run(Demo2Application.class, args);
 	}
 
